7145e130a9b08eb968341ad57b17ce0b85524e36
Plamen Terziev <pterziev@datexcorp.com>
2022-11-16 21:26:25 +0200

    Updated code

--- src/main/java/com/wordscore/engine/processor/ImportCsvFilePostJob.java
+++ src/main/java/com/wordscore/engine/processor/ImportCsvFilePostJob.java
@@ -9,7 +9,12 @@ import org.quartz.JobExecutionContext;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +27,17 @@ public class ImportCsvFilePostJob extends ServiceFactory implements Job {
     @Override
     public void execute(JobExecutionContext context) {
 
-        File directoryPath = new File("C:\\csv\\nov_3");
+        File directoryPath = new File("C:\\csv\\nov");
+        // Create a new subfolder called "processed" into source directory
+        try {
+            Path path = Path.of(directoryPath.getAbsolutePath() + "/processed");
+            if (!Files.exists(path) || !Files.isDirectory(path)) {
+                Files.createDirectory(path);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         FilenameFilter textFileFilter = (dir, name) -> {
             String lowercaseName = name.toLowerCase();
             if (lowercaseName.endsWith(".csv")) {
@@ -41,14 +56,16 @@ public class ImportCsvFilePostJob extends ServiceFactory implements Job {
 //            System.out.println(" ");
 
             try {
-                List<CsvLine> beans = new CsvToBeanBuilder(new FileReader(file.getAbsolutePath(), StandardCharsets.UTF_16))
-                        .withType(CsvLine.class)
-                        .withSeparator('\t')
-                        .withSkipLines(3)
-                        .build()
-                        .parse();
+                try (var br = new FileReader(file.getAbsolutePath(), StandardCharsets.UTF_16)){
+                    List<CsvLine> beans = new CsvToBeanBuilder(br)
+                            .withType(CsvLine.class)
+                            .withSeparator('\t')
+                            .withSkipLines(3)
+                            .build()
+                            .parse();
 
-                for(CsvLine item: beans){
+
+                for (CsvLine item : beans) {
 
                     System.out.println(String.format("Keyword: %s, avgMonthlySearches: %s, lowRange: %f, highRange: %f",
                             item.getKeyword(),
@@ -56,41 +73,48 @@ public class ImportCsvFilePostJob extends ServiceFactory implements Job {
                             item.getLowRange(),
                             item.getHighRange()));
 
-                        Optional<ProcessedWords> isFound = processedWordsService.findByKeyword(item.getKeyword());
+                    Optional<ProcessedWords> isFound = processedWordsService.findByKeyword(item.getKeyword());
 
-                        if(isFound.isPresent())
+                    if (isFound.isPresent()) {
+                        // Sometimes low range ang high range can be empty. We need to skip this case and update only ScoreUs
+                        if (item.getLowRange() != null && item.getHighRange() != null) {
+                            UpdateKeywordRequestDTO obj = UpdateKeywordRequestDTO.builder()
+                                    .keyword(item.getKeyword()).seoScoreUs(item.getAvgMonthlySearches())
+                                    .lowRange(item.getLowRange()).highRange(item.getHighRange()).build();
+                            processedWordsService.updateBySeoScoreUs(obj);
+                        } else // Update all values based on keyword
                         {
-                            // Sometimes low range ang high range can be empty. We need to skip this case and update only ScoreUs
-                            if(item.getLowRange() != null && item.getHighRange() != null)
-                            {
-                                UpdateKeywordRequestDTO obj = UpdateKeywordRequestDTO.builder()
-                                        .keyword(item.getKeyword()).seoScoreUs(item.getAvgMonthlySearches())
-                                        .lowRange(item.getLowRange()).highRange(item.getHighRange()).build();
-                                processedWordsService.updateBySeoScoreUs(obj);
-                            }
-                            else // Update all values based on keyword
-                            {
-                                UpdateKeywordRequestDTO obj = UpdateKeywordRequestDTO.builder()
-                                        .keyword(item.getKeyword()).seoScoreUs(item.getAvgMonthlySearches())
-                                        .lowRange(item.getLowRange()).highRange(item.getHighRange()).build();
-                                processedWordsService.update(obj);
-                            }
+                            UpdateKeywordRequestDTO obj = UpdateKeywordRequestDTO.builder()
+                                    .keyword(item.getKeyword()).seoScoreUs(item.getAvgMonthlySearches())
+                                    .lowRange(item.getLowRange()).highRange(item.getHighRange()).build();
+                            processedWordsService.update(obj);
                         }
-                        else // Insert all values as new db row
-                        {
-                            ProcessedWords obj = ProcessedWords.builder()
-                                    .keyword(item.getKeyword())
-                                    .seoScoreUs(item.getAvgMonthlySearches())
-                                    .lowRange(item.getLowRange())
-                                    .highRange(item.getHighRange())
-                                    .createdAt(LocalDateTime.now())
-                                    .build();
-                            processedWordsService.save(obj);
-                        }
-                }
+                    } else // Insert all values as new db row
+                    {
+                        ProcessedWords obj = ProcessedWords.builder()
+                                .keyword(item.getKeyword())
+                                .seoScoreUs(item.getAvgMonthlySearches())
+                                .lowRange(item.getLowRange())
+                                .highRange(item.getHighRange())
+                                .createdAt(LocalDateTime.now())
+                                .build();
+                        processedWordsService.save(obj);
+                    }
+
+
+            }}
 
             } catch (Exception e){
                 e.printStackTrace();
+            }
+
+            // Move here file into new subdirectory when file processing is finished
+            Path copied = Paths.get(file.getAbsolutePath() + "/processed");
+            Path originalPath = file.toPath();
+            try {
+                Files.move(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
